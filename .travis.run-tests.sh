a6cd9a3e75982856a220f17e0b7a66014f4c6470
Filippo Tessarotto <zoeslam@gmail.com>
2017-06-26 12:47:44 +0200

    xDebug unneded when no CC required

--- .travis.run-tests.sh
+++ .travis.run-tests.sh
@@ -8,5 +8,6 @@ then
     wget https://scrutinizer-ci.com/ocular.phar || return 0
     php ocular.phar code-coverage:upload --format=php-clover ./clover.xml || return 0
 else
+    phpenv config-rm xdebug.ini || return 0
     vendor/bin/phpunit
 fi
