106ee7c1672fe009c6e8808fe514a98f6afbb908
Filippo Tessarotto <zoeslam@gmail.com>
2017-06-26 15:25:34 +0200

    Travis optimization

--- .travis.install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -ev
-if [ "$PHP_LATEST_VERSION" != 1 ]
-then
-    composer remove --no-update --no-scripts --dev slam/php-cs-fixer-extensions
-fi
-composer install --prefer-dist
--- .travis.run-tests.sh
+++ .travis.run-tests.sh
@@ -2,12 +2,15 @@
 set -ev
 if [ "$PHP_LATEST_VERSION" = 1 ]
 then
+    composer install --prefer-dist
     vendor/bin/phpunit --coverage-clover ./clover.xml
     phpenv config-rm xdebug.ini || return 0
-    vendor/bin/php-cs-fixer --diff --dry-run --verbose fix
+    vendor/bin/php-cs-fixer fix --diff --dry-run --verbose
     wget https://scrutinizer-ci.com/ocular.phar || return 0
     php ocular.phar code-coverage:upload --format=php-clover ./clover.xml || return 0
 else
     phpenv config-rm xdebug.ini || return 0
+    composer remove --no-update --no-scripts --dev slam/php-cs-fixer-extensions
+    composer install --prefer-dist
     vendor/bin/phpunit
 fi
